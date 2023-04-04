292f7dc5dd94d82a64800552ca26ccf9fc64dd58
loginmarii <129678289+loginmarii@users.noreply.github.com>
2023-04-04 18:30:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1676035524 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return (np.sum(x) / x.shape + 32) / 10 # Ваш ответ
